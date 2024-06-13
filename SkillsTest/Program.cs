7b529e84a965f7a8b4ee09a3e79ded4aa1b6ffb1
Lasse Egebo <lae003@edu.zealand.dk>
2024-06-13 17:43:02 +0200

    Add project files.

---
+++ SkillsTest/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace SkillsTest
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
